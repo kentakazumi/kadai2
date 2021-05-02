ac2d0e684d47a59e0f1a5367931c9d1bad6c579a
kentakazumi <majorkzmkkzmk@gmail.com>
2021-05-02 09:32:38 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,71 @@
+class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
+   before_action :authenticate_user!
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      @user = current_user
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    if @user != current_user
+    redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  def destroy
+     @book = Book.find(params[:id])
+     @book.destroy
+     redirect_to "/books"
+  end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+    redirect_to books_path
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
